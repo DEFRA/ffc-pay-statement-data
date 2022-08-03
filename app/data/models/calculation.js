356efaf83ebe2404cb0778b711e71b87e1d88958
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-08-03 10:52:31 +0100

    Add initial database schema (#4)

---
+++ app/data/models/calculation.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const calculation = sequelize.define('calculation', {
+    calculationId: { type: DataTypes.INTEGER, primaryKey: true },
+    sbi: DataTypes.INTEGER,
+    frn: DataTypes.BIGINT,
+    calculationDate: DataTypes.DATE,
+    invoiceNumber: DataTypes.STRING,
+    scheme: DataTypes.STRING,
+    updated: DataTypes.DATE
+  },
+  {
+    tableName: 'calculations',
+    freezeTableName: true,
+    timestamps: false
+  })
+  calculation.associate = function (models) {
+    calculation.hasMany(models.funding, {
+      foreignKey: 'calculationId',
+      as: 'funding'
+    })
+    calculation.belongsTo(models.organisation, {
+      foreignKey: 'sbi',
+      as: 'organisations'
+    })
+  }
+  return calculation
+}
