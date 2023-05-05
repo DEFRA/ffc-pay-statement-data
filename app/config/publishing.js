f286858e76444abb5a29d71255d2daf77594bfbe
Marc Templeton <marcte@kainos.com>
2023-05-05 11:13:17 +0100

    adding publishing config with max batch size

---
+++ app/config/publishing.js
@@ -0,0 +1,19 @@
+const Joi = require('joi')
+
+const schema = Joi.object({
+  dataPublishingMaxBatchSize: Joi.number().default(500)
+})
+
+const config = {
+  dataPublishingMaxBatchSize: process.env.DATA_PUBLISHING_MAX_BATCH_SIZE
+}
+
+const result = schema.validate(config, {
+  abortEarly: false
+})
+
+if (result.error) {
+  throw new Error(`The publishing config is invalid. ${result.error.message}`)
+}
+
+module.exports = result.value
