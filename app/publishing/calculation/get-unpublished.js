bb358a2e168ed48c0449dd1180e10a7988cb9fb2
Marc Templeton <marcte@kainos.com>
2023-05-09 11:03:33 +0100

    added limit successfully

--- app/publishing/calculation/get-unpublished.js
+++ app/publishing/calculation/get-unpublished.js
@@ -1,27 +1,39 @@
 const db = require('../../data')
 
+const { publishingConfig } = require('../../config')
+
 const getUnpublished = async (transaction) => {
-  const calculations = await db.calculation.findAll({
-    where: {
-      [db.Sequelize.Op.or]: [
-        {
-          published: null
-        },
-        {
-          published: { [db.Sequelize.Op.lt]: db.sequelize.col('updated') }
-        }
-      ]
-    },
-    include: [{
-      model: db.funding,
-      as: 'fundings',
-      attributes: ['calculationId', 'fundingCode', 'areaClaimed', 'rate']
-    }],
-    attributes: ['calculationId', ['calculationId', 'calculationReference'], 'sbi', 'frn', 'calculationDate', 'invoiceNumber', 'scheme', 'updated'],
-    nest: true,
-    // raw: true,
-    transaction
-  })
+  let calculations
+  try {
+    calculations = await db.calculation.findAll({
+      // lock: true,
+      // skipLocked: true,
+      limit: publishingConfig.dataPublishingMaxBatchSize,
+      where: {
+        [db.Sequelize.Op.or]: [
+          {
+            published: null
+          },
+          {
+            published: { [db.Sequelize.Op.lt]: db.sequelize.col('updated') }
+          }
+        ]
+      },
+      include: [{
+        // lock: true,
+        // skipLocked: true,
+        limit: publishingConfig.dataPublishingMaxBatchSize,
+        model: db.funding,
+        as: 'fundings',
+        attributes: ['calculationId', 'fundingCode', 'areaClaimed', 'rate']
+      }],
+      attributes: ['calculationId', ['calculationId', 'calculationReference'], 'sbi', 'frn', 'calculationDate', 'invoiceNumber', 'scheme', 'updated'],
+      nest: true,
+      transaction
+    })
+  } catch (e) {
+    console.log(e)
+  }
 
   return calculations.map(x => x.get({ plain: true }))
 }
